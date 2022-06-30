4b2460fd36e974e2ccb77212083b8f8af1833b5f
Neko <hoiyameh@gmail.com>
2022-06-29 19:07:32 -0700

    Fixed old version axios vuln
    Removed battlefy-api as a dependency due to its outdated dependencies instead preferred including in index

--- index.js
+++ index.js
@@ -1,7 +1,47 @@
+const axios = require('axios');
+const searchApi = axios.create({
+    baseURL: 'https://search.battlefy.com/'
+})
+const tournamentApi = axios.create({
+    baseURL: 'https://dtmwra1jsgyb0.cloudfront.net/'
+})
+const bfa = {
+    getPopularGames: async () => {
+        const response = await searchApi.get('game/popular');
+        return response.data;
+    },
+    getGameTournaments: async (gameId) => {
+        const response = await searchApi.get(`tournament/browse/${gameId}`);
+        return response.data.tournaments;
+    },
+    getStaffPicks: async () => {
+        const response = await searchApi.get(`spotlight?type=discovery`);
+        return response.data;
+    },
+    getTournamentData: async (id) => {
+        const response = await tournamentApi.get(`tournaments/${id}`);
+        return response.data;
+    },
+    getTournamentStageData: async (stage) => {
+        const response = await tournamentApi.get(`stages/${stage}`);
+        return response.data;
+    },
+    getTournamentStageMatches: async (stage) => {
+        const response = await tournamentApi.get(`stages/${stage}/matches`);
+        for (match in response.data) {
+            var winner = response.data[match].top.winner ? 'top' : 'bottom'
+            response.data[match].winner = winner;
+        }
+        return response.data;
+    },
+    getTournamentTeams: async (id) => {
+        const response = await tournamentApi.get(`tournaments/${id}/teams`);
+        return response.data;
+    }
+}
 const https = require('https');
 const tID = ''; //Put tournament ID here https://battlefy.com/team-tsd/overunder-tetrio-doubles/<id>
 const fs = require('fs');
-bfa = require("battlefy-api");
 function getPromise(path)
 {
 	return new Promise((resolve, reject) => {
